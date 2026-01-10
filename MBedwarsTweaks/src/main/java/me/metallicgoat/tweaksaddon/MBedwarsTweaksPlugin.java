33a009675c65df51977b9460030f1baf48435640
MetallicGoat <ecazzam3@gmail.com>
2026-01-10 10:57:22 -0500

    v5.0.0

--- MBedwarsTweaks/src/main/java/me/metallicgoat/tweaksaddon/MBedwarsTweaksPlugin.java
+++ MBedwarsTweaks/src/main/java/me/metallicgoat/tweaksaddon/MBedwarsTweaksPlugin.java
@@ -20,8 +20,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MBedwarsTweaksPlugin extends JavaPlugin {
 
-  public static final int MIN_MBEDWARS_API_VER = 205;
-  public static final String MIN_MBEDWARS_VER_NAME = "5.5.5";
+  public static final int MIN_MBEDWARS_API_VER = 206;
+  public static final String MIN_MBEDWARS_VER_NAME = "5.5.6";
 
   @Getter
   private static MBedwarsTweaksPlugin instance;
